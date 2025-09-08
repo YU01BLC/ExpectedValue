32a6ba21ef67846c511e06d7cef5ce1b3b2fabbb
yusukeMIYAUCHI <yusukemiyauchi@MBA.local>
2025-09-09 00:58:04 +0900

    update: 構成見直し

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router';
+import AppProviders from './App.tsx';
+import { router } from './routes/index.tsx';
 
 // index.html で #root 要素の存在が保証されているため Non-null アサーションは安全です。
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </StrictMode>
 );
