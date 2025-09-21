dafc3934616e8e54c2a311eb3282ed1729fdfade
yusukeMIYAUCHI <yusukemiyauchi@MBA.local>
2025-09-21 10:48:35 +0900

    add: 配布用ディレクトリ作成

---
+++ dist-files/pwa/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'expected-value-v1.1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+// インストール時の処理
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// フェッチ時の処理
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがある場合はそれを返す
+        if (response) {
+          return response;
+        }
+        // キャッシュがない場合はネットワークから取得
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// アクティベート時の処理
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
